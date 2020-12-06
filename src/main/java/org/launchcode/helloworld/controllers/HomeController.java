77429e06bc56f8866cf68dcca750c484d7f42ef6
Taylor Branson <taylor.a.branson@gmail.com>
2020-12-06 01:29:32 -0600

    removed references to helloworld, refactored to brewpub

--- src/main/java/org/launchcode/helloworld/controllers/HomeController.java
+++ src/main/java/org/launchcode/helloworld/controllers/HomeController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HelloController {
+public class HomeController {
 
 
     @RequestMapping("")
