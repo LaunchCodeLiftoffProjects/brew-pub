32e89a41322d0eb396046f8f209a28fe6e2abe53
Taylor Branson <taylor.a.branson@gmail.com>
2020-12-06 01:25:56 -0600

    removed references to helloworld, refactored to brewpub

--- src/test/java/org/launchcode/helloworld/BrewPubApplicationTests.java
+++ src/test/java/org/launchcode/helloworld/BrewPubApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HelloWorldApplicationTests {
+class BrewPubApplicationTests {
 
 	@Test
 	void contextLoads() {
