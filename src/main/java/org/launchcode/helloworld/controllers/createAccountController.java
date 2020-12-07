3d2e658f5c1d0fec325cf6cedfdb127b7f373097
Austin Thomas <thomasaustin78@gmail.com>
2020-12-07 17:43:01 -0600

    changed html in accountCreation.html added birthday and gender feature and button to navigate screens

--- src/main/java/org/launchcode/helloworld/controllers/createAccountController.java
+++ src/main/java/org/launchcode/helloworld/controllers/createAccountController.java
@@ -2,6 +2,9 @@ package org.launchcode.helloworld.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class createAccountController {
