de2be697c343623c1bda85989b9289e15f3debe6
jonathan meeth <jjmeeth@gmail.com>
2020-12-07 20:07:14 -0600

    Add userLogin template and controller

---
+++ src/main/java/org/launchcode/helloworld/controllers/UserLoginController.java
@@ -0,0 +1,14 @@
+package org.launchcode.helloworld.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserLoginController {
+
+    @RequestMapping("/userLogin")
+    public String displayUserLoginForm() {
+
+        return "userLogin";
+    }
+}
