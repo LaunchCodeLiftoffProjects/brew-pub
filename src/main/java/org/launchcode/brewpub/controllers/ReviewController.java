a23be0ac2f188747aed18075f04187da8245a425
Taylor Branson <taylor.a.branson@gmail.com>
2020-12-14 16:01:54 -0600

    passed Pub info to view

--- src/main/java/org/launchcode/brewpub/controllers/ReviewController.java
+++ src/main/java/org/launchcode/brewpub/controllers/ReviewController.java
@@ -1,20 +1,20 @@
 package org.launchcode.brewpub.controllers;
 
 import org.launchcode.brewpub.models.BrewReview;
+import org.launchcode.brewpub.models.Pub;
 import org.launchcode.brewpub.models.Review;
 import org.launchcode.brewpub.models.data.BrewReviewRepository;
+import org.launchcode.brewpub.models.data.PubRepository;
 import org.launchcode.brewpub.models.data.PubReviewRepository;
 import org.launchcode.brewpub.models.PubReview;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("review")
@@ -26,28 +26,35 @@ public class ReviewController {
     @Autowired
     private PubReviewRepository pubReviewRepository;
 
+    @Autowired
+    private PubRepository pubRepository;
+
 
     // Pub Review
 
-    @GetMapping("pub"
-//            + "/{pubId}"
-    )
-    public String viewPubReviewForm(Model model
-//                                , @PathVariable int pubId
-                                ) {
-        // TODO: pass-in brew-pub info to model using pubId
+    @GetMapping("pub" + "/{pubId}")
+    public String viewPubReviewForm(Model model, @PathVariable Integer pubId) {
         // TODO: pass-in userId using session ID and session cookie to look up
-        model.addAttribute("title", "Review");
-        model.addAttribute("brewpub", "name-of-a-brewpub");
-
-        model.addAttribute("pubReview", new PubReview());
-        return "reviews/reviewPub";
+        if (pubId.equals(null)) {
+            return "pubs/index";
+        } else {
+            Optional<Pub> result = pubRepository.findById(pubId);
+            if (result.isEmpty()) {
+                return "pubs/index";
+            } else {
+                Pub pub = result.get();
+                model.addAttribute("brewpub", pub);
+                model.addAttribute("pubReview", new PubReview());
+            }
+            return "reviews/reviewPub";
+        }
     }
 
     @PostMapping("pub")
     public String processPubReviewForm(@ModelAttribute @Valid PubReview newPubReview,
-                                    Errors errors,
-                                    Model model) {
+                                       Errors errors,
+                                       Model model,
+                                       @RequestParam int pubId) {
         if (errors.hasErrors()) {
             model.addAttribute("title", "Review");
             model.addAttribute("brewpub", "name-of-a-brewpub");
@@ -57,9 +64,13 @@ public class ReviewController {
 //            model.addAttribute("pubId",pubId);
 
             return "reviews/reviewPub";
-        }
+        } else {
+            Optional<Pub> result = pubRepository.findById(pubId);
+            Pub pub = result.get();
+            newPubReview.setPub(pub);
 
-        pubReviewRepository.save(newPubReview);
+            pubReviewRepository.save(newPubReview);
+        }
 
         // TODO: return (redirect) to brew-pub detail view
         return "redirect:/";
