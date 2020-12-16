e5117554bdb9c1fd9611e34b57e5c82201a09c15
Taylor Branson <taylor.a.branson@gmail.com>
2020-12-15 18:04:32 -0600

    added db search findAllByPubId()

--- src/main/java/org/launchcode/brewpub/models/data/PubReviewRepository.java
+++ src/main/java/org/launchcode/brewpub/models/data/PubReviewRepository.java
@@ -5,6 +5,9 @@ import org.launchcode.brewpub.models.Review;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PubReviewRepository extends CrudRepository<PubReview, Integer> {
+    List<PubReview> findAllByPubId(int pubId);
 }
