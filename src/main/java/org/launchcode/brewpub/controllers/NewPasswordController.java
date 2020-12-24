c17c2b0b6dc8bbca88cd22204d5240c67f24f510
Aby <abyssiniaasfaw@gmail.com>
2020-12-23 18:57:32 -0600

    in progress

--- src/main/java/org/launchcode/brewpub/controllers/NewPasswordController.java
+++ src/main/java/org/launchcode/brewpub/controllers/NewPasswordController.java
@@ -15,20 +15,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 @Controller
-public class newPasswordController {
+public class NewPasswordController {
 
     @Autowired
     UserRepository userRepository;
 
     @GetMapping("/forgotPassword")
-    public String viewForgotPasswordForm(Model model){
+    public String viewForgotPasswordForm(Model model) {
         model.addAttribute("title", "forgotPassword");
         model.addAttribute(new NewPasswordDTO());
         return "forgotPassword";
     }
 
     @PostMapping("/newPassword")
-    public String newPasswordForm(Model model, NewPasswordDTO newPasswordDTO){
+    public String newPasswordForm(Model model, NewPasswordDTO newPasswordDTO) {
         model.addAttribute("title", "newPassword");
         User existingUser = userRepository.findByEmail(newPasswordDTO.getEmail());
         if (existingUser == null) {
@@ -38,14 +38,15 @@ public class newPasswordController {
         return "newPassword";
     }
 
-    @PostMapping
-    public String processNewPasswordForm(@ModelAttribute @Valid NewPasswordDTO newPasswordDTO, Errors errors, Model model){
+    @PostMapping("")
+    public String processNewPasswordForm(@ModelAttribute @Valid NewPasswordDTO newPasswordDTO, Errors errors, Model model) {
         String password = newPasswordDTO.getPassword();
         String verifyPassword = newPasswordDTO.getVerifyPassword();
 
         if (password.equals(verifyPassword)) {
 
         }
-    }
+        return "did it";
 
+    }
 }
