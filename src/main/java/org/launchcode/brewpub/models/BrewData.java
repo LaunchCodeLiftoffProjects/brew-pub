400555843a3eb8e04c1729fac17b062ca78f543f
unknown <xmdanh3@gmail.com>
2020-12-28 17:37:46 -0600

    created BrewData file

---
+++ src/main/java/org/launchcode/brewpub/models/BrewData.java
@@ -0,0 +1,82 @@
+package org.launchcode.brewpub.models;
+
+import java.util.ArrayList;
+
+public class BrewData {
+    /**
+     * Returns the results of searching the Jobs data by field and search term.
+     *
+     * For example, searching for employer "Enterprise" will include results
+     * with "Enterprise Holdings, Inc".
+     *
+     * @param column Job field that should be searched.
+     * @param value Value of the field to search for.
+     * @param allBrews The list of brews to search.
+     * @return List of all jobs matching the criteria.
+     */
+
+    public static ArrayList<Brew> findByColumnAndValue(String column, String value, Iterable<Brew> allBrews) {
+
+        ArrayList<Brew> results = new ArrayList<>();
+
+        if (value.toLowerCase().equals("all")) {
+            return (ArrayList<Brew>) allBrews;
+        }
+
+        if (column.equals("all")) {
+            results = findByValue(value, allBrews);
+            return results;
+        }
+
+        for (Brew brew : allBrews) {
+
+            String aValue = getFieldValue(brew, column);
+
+            if(aValue != null && aValue.toLowerCase().contains(value.toLowerCase())) {
+                results.add(brew);
+            }
+        }
+        return results;
+    }
+
+    public static String getFieldValue(Brew brew, String fieldName) {
+        String theValue = "";
+        if (fieldName.equals("name")) {
+            theValue = brew.getName();
+        } else if (fieldName.equals("style")) {
+            theValue = brew.getStyle().toString();
+        }
+
+        return theValue;
+    }
+
+    /**
+     * Search all Job fields for the given term.
+     *
+     * @param value The search term to look for.
+     * @param allBrews The list of jobs to search.
+     * @return      List of all jobs with at least one field containing the value.
+     */
+
+    public static ArrayList<Brew> findByValue(String value, Iterable<Brew> allBrews) {
+        String lower_val = value.toLowerCase();
+
+        ArrayList<Brew> results = new ArrayList<>();
+
+        for (Brew brew : allBrews) {
+            if (brew.getName().toLowerCase().contains(lower_val)) {
+                results.add(brew);
+            } else if (brew.getStyle().toString().toLowerCase().contains(lower_val)) {
+                results.add(brew);
+            }
+
+        }
+
+        return results;
+    }
+
+
+
+}
+
+
