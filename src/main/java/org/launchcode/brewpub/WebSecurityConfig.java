17bbf1c22c86c6875fc0d845c6e3452d53127770
Taylor Branson <taylor.a.branson@gmail.com>
2021-01-05 13:21:16 -0600

    added forgotPassword

--- src/main/java/org/launchcode/brewpub/WebSecurityConfig.java
+++ src/main/java/org/launchcode/brewpub/WebSecurityConfig.java
@@ -24,7 +24,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/", "/createAccount", "/css/**", "/js/**").permitAll()
+                    .antMatchers("/", "/createAccount", "/forgotPassword", "/css/**", "/js/**").permitAll()
                     .anyRequest().authenticated()
                     .and()
                 .formLogin()
