e154eb7586ed0aea6f2fc865ee1c31f298661558
unknown <xmdanh3@gmail.com>
2021-01-06 19:48:21 -0600

    brew view linked to search results

--- src/main/java/org/launchcode/brewpub/WebSecurityConfig.java
+++ src/main/java/org/launchcode/brewpub/WebSecurityConfig.java
@@ -24,7 +24,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/", "/createAccount", "/forgotPassword", "/css/**", "/js/**").permitAll()
+                    .antMatchers("/", "/createAccount", "/forgotPassword","/search/**", "/css/**", "/js/**").permitAll()
                     .anyRequest().authenticated()
                     .and()
                 .formLogin()
