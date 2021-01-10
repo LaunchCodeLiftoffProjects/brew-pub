2b51ad73d1fbda3529149bfd933b3ae392e96a19
Taylor Branson <taylor.a.branson@gmail.com>
2021-01-10 00:32:36 -0600

    implemented basic integration tests

--- src/test/java/org/launchcode/brewpub/BrewPubApplicationTests.java
+++ src/test/java/org/launchcode/brewpub/BrewPubApplicationTests.java
@@ -1,13 +1,25 @@
 package org.launchcode.brewpub;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.launchcode.brewpub.controllers.HomeController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BrewPubApplicationTests {
 
+	@Autowired
+	private HomeController homeController;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void homeControllerLoads() {
+		assertThat(homeController).isNotNull();
 	}
 
 }
