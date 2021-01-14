ecc42531a971a7ea3726b2cca1339a72d95b03e1
Taylor Branson <taylor.a.branson@gmail.com>
2021-01-14 16:13:56 -0600

    added getFiveMostRecent()

--- src/main/java/org/launchcode/brewpub/controllers/HomeController.java
+++ src/main/java/org/launchcode/brewpub/controllers/HomeController.java
@@ -1,6 +1,8 @@
 package org.launchcode.brewpub.controllers;
 
 import org.launchcode.brewpub.models.AbstractEntity;
+import org.launchcode.brewpub.models.Brew;
+import org.launchcode.brewpub.models.Pub;
 import org.launchcode.brewpub.models.data.BrewRepository;
 import org.launchcode.brewpub.models.data.PubRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Controller
 public class HomeController {
@@ -22,9 +23,16 @@ public class HomeController {
 
     @RequestMapping("")
     public String index(Model model) {
+        List<Pub> allPubs = pubRepository.findAll();
+        List<Brew> allBrews = brewRepository.findAll();
+
         model.addAttribute("title","Home");
-        model.addAttribute("recentPubs", getFiveMostRecent(pubRepository.findAll()));
-        model.addAttribute("recentBrews", getFiveMostRecent(brewRepository.findAll()));
+
+        model.addAttribute("recentPubs", getFiveMostRecent(allPubs));
+        model.addAttribute("recentBrews", getFiveMostRecent(allBrews));
+
+        model.addAttribute("popularPubs", getFiveMostPopular(allPubs));
+        model.addAttribute("popularBrews", getFiveMostPopular(allBrews));
         return "index";
     }
 
@@ -41,6 +49,57 @@ public class HomeController {
                 fiveMostRecent.add(items.get(i));
             }
             return fiveMostRecent;
+        }
+    }
+
+    private List<? extends AbstractEntity> getFiveMostPopular(List<? extends AbstractEntity> items) {
+        if (items == null || items.isEmpty()) {
+            List<AbstractEntity> empty = new ArrayList<>();
+            return empty;
+        } else {
+            List<AbstractEntity> fiveMostPopular = new ArrayList<>();
+            int length = (items.size() >= 5) ? 5 : items.size();
+
+            if (items.get(0).getClass() == Brew.class) {
+                List<Brew> temp = new ArrayList<>();
+
+                for (AbstractEntity item : items) {
+                    temp.add((Brew) item);
+                }
+
+                Collections.sort(temp, (o1, o2) -> {
+                    Integer sizeBrewOne = o1.getBrewFavoriteUser().size();
+                    Integer sizeBrewTwo = o2.getBrewFavoriteUser().size();
+
+                    return sizeBrewTwo - sizeBrewOne;
+                });
+
+                for (int i = 0; i < length; i++) {
+                    fiveMostPopular.add(temp.get(i));
+                }
+
+            } else if (items.get(0).getClass() == Pub.class) {
+                List<Pub> temp = new ArrayList<>();
+
+                for (AbstractEntity item : items) {
+                    temp.add((Pub) item);
+                }
+
+                Collections.sort(temp, (o1, o2) -> {
+                    Integer sizeBrewOne = o1.getPubFavoriteUser().size();
+                    Integer sizeBrewTwo = o2.getPubFavoriteUser().size();
+
+                    return sizeBrewTwo - sizeBrewOne;
+                });
+
+                for (int i = 0; i < length; i++) {
+                    fiveMostPopular.add(temp.get(i));
+                }
+
+                return fiveMostPopular;
+            }
+
+            return fiveMostPopular;
         }
     }
 
