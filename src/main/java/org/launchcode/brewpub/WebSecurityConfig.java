6c2c538f721d32a3725a7258248d505a3087a1da
Taylor Branson <taylor.a.branson@gmail.com>
2021-01-27 12:28:03 -0600

    allow unauthenticated users to view /uploads/**

--- src/main/java/org/launchcode/brewpub/WebSecurityConfig.java
+++ src/main/java/org/launchcode/brewpub/WebSecurityConfig.java
@@ -23,7 +23,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/", "/createAccount", "/forgotPassword", "/newPassword", "/resetPassword", "/pubs", "/pubs/view/**", "/pubs/brews/view/**", "/userProfile/**", "/search/**").permitAll()
+                    .antMatchers("/", "/createAccount", "/forgotPassword", "/newPassword", "/resetPassword", "/pubs", "/pubs/view/**", "/pubs/brews/view/**", "/userProfile/**", "/search/**", "/uploads/**").permitAll()
                     .anyRequest().authenticated()
                     .and()
                 .formLogin()
